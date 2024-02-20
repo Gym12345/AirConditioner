60ff6ef6bad4ee7873f3c6416da9712aa08baa75
jun <pickjog@gmail.com>
2024-02-20 16:11:10 +0900

    jun설정 끝

---
+++ AirConditioner/src/jun_package/Change_temperature.java
@@ -0,0 +1,24 @@
+package jun_package;
+
+import main.AirconditionerDTO;
+
+public class Change_temperature  extends AirconditionerDTO implements interface_airConditioner.Change_temperature{
+	
+	@Override
+	public int change_temperature(int choice) {
+		int num;
+		if(choice==1) {
+			 setTemperature(num=(choice+=1));
+			 System.out.println("현재 온도는"+num+"도 입니다.");
+	}
+	else if(choice==2) {
+			setTemperature(num=choice-=1);
+			 System.out.println("현재 온도는"+num+"도 입니다.");
+	}
+	else {
+		System.out.println("wrong input");
+	}
+		return 0;
+	}
+
+}
